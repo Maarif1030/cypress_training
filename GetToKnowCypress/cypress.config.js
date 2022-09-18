f65fd8b4273d2ad8995357fe1297d9f2349b681b
Abul Maarif <abdulmaarif@A006-00462.local>
2022-09-18 16:16:26 +0500

    inital commit

--- GetToKnowCypress/cypress.config.js
+++ GetToKnowCypress/cypress.config.js
@@ -3,21 +3,25 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
-      if (config.env.ENVIRONMENT == "session1"){
+      //implement node event listeners here
+      if (config.env.ENVIRONMENT == "session1") {
         return {
           baseUrl: "https://studio.stage.edx.org",
-          specPattern: "cypress/e2e/session1/**/*.cy.js"
-        }
-      }
-      else if (config.env.ENVIRONMENT == "session2"){
+          specPattern: "cypress/e2e/session1/**/*.cy.js",
+        };
+      } else if (config.env.ENVIRONMENT == "session2") {
         return {
-          baseUrl: "https://www.saucedemo.com/",
+          baseUrl: "http://the-internet.herokuapp.com/",
           specPattern: "cypress/e2e/session2/**/*.cy.js",
-          chromeWebSecurity: false
-        }
+          chromeWebSecurity: false,
+        };
+      } else if (config.env.ENVIRONMENT == "assignment1") {
+        return {
+          baseUrl: "http://the-internet.herokuapp.com/",
+          specPattern: "cypress/e2e/Assignment1/**/*.cy.js",
+          chromeWebSecurity: false,
+        };
       }
-
     },
   },
 });
---
+++ GetToKnowCypress/cypress/e2e/assignment1/assignment1.cy.js
@@ -0,0 +1,11 @@
+/// <reference types="cypress" />
+
+describe("login, dropdown, add element, loop through images ", () => {
+  before(() => {
+    cy.visit("/login");
+  });
+
+  it("verify login with valid credentials", () => {
+    //cy.get(".")
+  });
+});
